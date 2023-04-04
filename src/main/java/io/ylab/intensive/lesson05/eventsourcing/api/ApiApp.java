794ec4cb24b76773baf254bc3fa8b71b57cc3dde
max <maximus235711@rambler.ru>
2023-04-04 23:09:15 +0300

    add homework5

--- src/main/java/io/ylab/intensive/lesson05/eventsourcing/api/ApiApp.java
+++ src/main/java/io/ylab/intensive/lesson05/eventsourcing/api/ApiApp.java
@@ -1,5 +1,6 @@
 package io.ylab.intensive.lesson05.eventsourcing.api;
 
+import io.ylab.intensive.lesson05.eventsourcing.Person;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ApiApp {
@@ -9,5 +10,29 @@ public class ApiApp {
     applicationContext.start();
     PersonApi personApi = applicationContext.getBean(PersonApi.class);
     // пишем взаимодействие с PersonApi
+    personApi.deletePerson(100L);
+    personApi.savePerson(1L, "Darya", "Ivanova", "Iv");
+    personApi.savePerson(2L, "Maria", "M", "Ma");
+    personApi.savePerson(3L, "Margo", "G", "Ge");
+    Thread.sleep(2000);
+    Person person = personApi.findPerson(1L);
+    showPerson(person);
+    System.out.println("=================");
+    for (Person person1 : personApi.findAll()){
+      showPerson(person1);
+    }
+    System.out.println("==================");
+    personApi.deletePerson(2L);
+    Thread.sleep(2000);
+    for (Person person1 : personApi.findAll()){
+      showPerson(person1);
+    }
+    applicationContext.close();
+  }
+
+  private static void showPerson(Person person){
+    if(person != null) {
+      System.out.println(person.getId() + " " + person.getName() + " " + person.getLastName() + " " + person.getMiddleName());
+    }
   }
 }
